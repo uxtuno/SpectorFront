62d23e3a7372441492ff6ba5c8cdabebe55b0442
uxtuno <uxtuno664@gmail.com>
2016-07-18 00:18:16 +0900

    UEにより自動的に追加されたアセットをコミット (スターターコンテンツなど)

---
+++ Source/SpecterFront/SpecterFrontGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "SpecterFront.h"
+#include "SpecterFrontGameMode.h"
+#include "SpecterFrontHUD.h"
+#include "SpecterFrontCharacter.h"
+
+ASpecterFrontGameMode::ASpecterFrontGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ASpecterFrontHUD::StaticClass();
+}
