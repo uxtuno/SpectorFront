49db52d08718cabeda3658b6cc03c8b4d03917fc
DESKTOP-NVJO8FT\uxtuno <uxtuno664@gmail.com>
2017-01-29 04:31:02 +0900

    ボスステージ仮実装

--- Source/SpecterFront/Private/GameFlow/SpawnDirector.cpp
+++ Source/SpecterFront/Private/GameFlow/SpawnDirector.cpp
@@ -2,16 +2,21 @@
 
 #include "SpecterFront.h"
 #include "SpawnDirector.h"
-//#include "Runtime/Engine/Classes/Components/BillboardComponent.h"
-//#include "Runtime/Engine/Classes/Components/ArrowComponent.h"
 
+#ifdef WITH_EDITOR
+#include "Runtime/Engine/Classes/Components/BillboardComponent.h"
+#include "Runtime/Engine/Classes/Components/ArrowComponent.h"
+#endif
 
 // Sets default values
 ASpawnDirector::ASpawnDirector()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	// スプライトを削除
+#ifdef WITH_EDITOR
 	//GetSpriteComponent()->DestroyComponent();
-	//GetArrowComponent()->DestroyComponent();
+	GetSpriteComponent()->SetVisibility(false, false);
+	GetArrowComponent()->SetVisibility(false, false);
+#endif
 }
