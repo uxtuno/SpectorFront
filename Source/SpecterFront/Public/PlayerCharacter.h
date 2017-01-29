3ae88f797040ea5d2146a2104bcd2393517b40f8
DESKTOP-NVJO8FT\uxtuno <uxtuno664@gmail.com>
2017-01-29 22:55:16 +0900

    コミット

--- Source/SpecterFront/Public/PlayerCharacter.h
+++ Source/SpecterFront/Public/PlayerCharacter.h
@@ -103,6 +103,10 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Player")
 		UBaseCameraController* GetCurrentCameraController() { return currentCameraController; }
 
+	// シーンチェンジ時に必ず呼ぶこと
+	UFUNCTION(BlueprintCallable, Category = "Player")
+		void OnLevelChanged();
+
 protected:
 
 	/** Fires a projectile. */
@@ -169,6 +173,9 @@ public:
 	// HP
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Status)
 		float hp;
+	// HP
+	UPROPERTY(BlueprintReadOnly, Category = Status)
+		float maxHp;
 
 	// 攻撃力
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Status)
